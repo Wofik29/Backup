f2df94a62bef7237b9dd183e2399333a9fc970c0
Wolf <wolf@daniil.ase.local>
2015-10-15 16:49:42 +0500

    Добавлен частичный бэкап.
    Работают два вида бэкапа. Добавлена информация в readme. Работает логирование.

--- src/Backup/Backup.java
+++ src/Backup/Backup.java
@@ -2,20 +2,16 @@ package Backup;
 
 import java.io.File;
 import java.util.List;
-import java.io.FileOutputStream;
-import java.io.ObjectOutputStream;
-import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.FileTime;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
 import java.util.Iterator;
+import java.util.logging.Level;
 
 /*
  * Объект, который работает с бэкапом.
@@ -25,77 +21,46 @@ public class Backup {
 	
 	private String storage;				// От куда брать
 	private String backup;				// Куда сохранять
-	private String history; 			// файлова история
+	private String history; 			// файловая история
 	
 	private boolean isFull;				// Флаг. Полный или частичный бэкап
-	
-	/*
-	 *  индекс символа, с которого брать относительный путь, для переноса файла в бэкап
-	 *  
-	 *  Например:  
-	 *  /Something/path/Storage/Something_dir/core.functions1.php
-	 *		В данном случает этим индексом будет "e" в слове storage. т.е. остальной путь мы копируем
-	 *  и присоединяем в этому пути.
-	 *	/Path/To/Backup/Files/Something_dir/core.functions1.php
-	 *  
-	 */
-	private int num_of_path; 
 	
 	private List<File> storage_dirs;	// список директории 
 	private List<File> storage_files;	// список файлов
 	private List<File> backup_dirs;		// список директории бэкапа
 	private List<File> backup_files;	// список файлов бэкапа
 	
-//	private Date now_time;			
-//	private Date last_time;
-//	private Date last_full_backup;
-	
-//	private File storage_file;		
-//	private File backup_file;		
-//	private File history_file;		 
-	
 	
 	public Backup(String backup, String storage, boolean isFull)
 		{
 			this.storage = storage;
-			num_of_path = storage.length();
 			this.backup = backup+File.separator+"current";;
 			this.history = backup+File.separator+"history";
 			this.isFull = isFull;
-			
+						
 			backup_dirs = new ArrayList<File>();
 			backup_files = new ArrayList<File>();
 			storage_dirs = new ArrayList<File>();
-			storage_files = new ArrayList<File>();
-			
-/*			
-			backup_file = new File(this.backup);
-			if (!backup_file.exists()) backup_file.mkdir();
-			
-			history_file = new File(this.history);
-			if (!history_file.exists()) history_file.mkdir();
-			
-			storage_file = new File(this.storage);
-*/
+			storage_files = new ArrayList<File>();	
 		}
 	
 	public void start()
 		{
 			
-			// Тут проверка на то, какой бэкап делать полный\частичный
+			// Проверка на то, какой бэкап делать полный\частичный
 			File backup_handler = new File(backup);
 			if (!(backup_handler.exists()) || isFull)
 			{
-				System.out.println("Full");
-				//Main.log.info("Start full backup");
+				Main.log.info("Start full backup");
 				backup_handler.mkdir();
 				copyFiles(new File(backup), new File(storage));
-				//Main.log.info("End full backup");
+				Main.log.info("End full backup");
 			}
 			else
 			{
-				System.out.println("noFull");
+				Main.log.info("Start additional backup");
 				additionalBackup(new File(backup), new File(storage), new File(history));
+				Main.log.info("Start additional backup");
 			}
 			//save();
 		}
@@ -105,9 +70,6 @@ public class Backup {
 	// Создание списка файлов в бэкапе
 	public void createListBackupFiles(File f, List<File> dirs, List<File> files)
 	{
-
-		//System.out.println(f.getPath());
-		//System.out.println(f.listFiles());
 		for (File file : f.listFiles())
 		{
 			if (file.isDirectory()) 
@@ -120,40 +82,21 @@ public class Backup {
 				files.add(file);
 			}
 		}
-		
-	}
-	
-	// Проверка на совпадение файл из backup
-	public boolean checkoutFile(Iterator<File> it, File file)
-	{
-		// пробегать по масиву dirs\files на поиск соответствия.
-		while (it.hasNext())
-		{
-			File f = (File) it.next();
-			
-			if (f.getName().equals(file.getName()) && f.lastModified()<file.lastModified() )
-			{
-				return true;
-			}
-			else
-			{
-				
-			}
-		}
-		
-		
-		return false;
 	}
 	
 	public void additionalBackup(File bp, File stg, File history)
 	{
+		if (!history.exists()) history.mkdir(); // Создание папки с историей, если нет
+		
+		// Создание списка файлов и папок бэкапа и хранилища
 		createListBackupFiles(bp, backup_dirs, backup_files);
 		createListBackupFiles(stg, storage_dirs, storage_files);
 		
+		
 		Collections.sort(backup_files);
-		Collections.sort(backup_dirs);
+		//Collections.sort(backup_dirs);
 		Collections.sort(storage_files);
-		Collections.sort(storage_dirs);
+		//Collections.sort(storage_dirs);
 		
 		Iterator<File> backup_it = backup_files.iterator();
 		Iterator<File> storage_it = storage_files.iterator();
@@ -164,71 +107,101 @@ public class Backup {
 // Много if. Как уменьшить?
 		while (storage_it.hasNext())
 		{
+			// Проверка на временный файл
+			if (storage_file.getName().substring(storage_file.getName().length()-1).equals("~"))
+			{
+				storage_file = storage_it.next();
+				continue;
+			}
+			
+			
 			if (backup_it.hasNext())
 			{
-				if (backup_file.getName() != storage_file.getName()) 
+				if (backup_file.getName().equals(storage_file.getName())) 
 				{
+					// Если именна файлов совпадают, но файл из хранилище новее, то копировать
+					if (backup_file.lastModified() < storage_file.lastModified()) 
+					{
+						// Создаем имя файла с временем изменения
+						DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd-HH:mm");
+					    String s = formatter.format(new Date(backup_file.lastModified()) );
+					    String name = backup_file.getName();
+					    name = name.substring(0, name.lastIndexOf('.')+1)+s+'.'+name.substring(name.lastIndexOf('.')+1);
+					    
+						//копируем из бэкапа в историю
+					    String path = backup_file.getPath().replaceAll(backup, history.getPath());
+					    path = path.replaceAll(backup_file.getName(), name); 
+					    //System.out.println("Copy to history : "+ backup_file.toString() + " - "+path);
+					    
+					    try 
+					    {
+					    	Files.copy(backup_file.toPath(), new File(path).toPath());
+						} 
+					    catch (Exception e) 
+						{
+							e.printStackTrace();
+						}
+					    backup_file.delete();
+					    
+					    // копируем из сторэдж в бэкап
+					    path = storage_file.getPath().replaceAll(storage, backup);					    					    
+					    
+					    //System.out.println("Copy to backup : "+ storage_file.toString() + " - "+path);
+					    
+					    try 
+					    {
+					    	File f = new File(path);
+					    	if (!f.exists()) Files.copy(storage_file.toPath(), f.toPath());
+						} 
+					    catch (Exception e) 
+						{
+							e.printStackTrace();
+						}
+					    
+					}
+					// TODO проверка, одинаковый ли путь. Т.е. файл может быть тот же, но в другом месте (случайный перенос) 
+					
+					// System.out.println("equal: " + storage_file.toString());
+					storage_file = storage_it.next();
+					backup_file = backup_it.next();
+				}
+				else
+				{
+					// Если файлы не совпали по имени, то проверяется, новый ли файл в хранилизеще
 					try
 					{
 						FileTime ft = Files.readAttributes(storage_file.toPath(), BasicFileAttributes.class).creationTime();
-						if (ft.toMillis() - ( new Date().getTime()) <= 1000*60*60*24 ) // Если файл создан за последние сутки, то копируем как новый  
+						if (ft.toMillis() - ( new Date().getTime()) >= 1000*60*60*24 ) // Если файл создан за последние сутки, то копируем как новый  
 						{
-							String path = backup_file.getPath().substring(num_of_path);
-							Files.copy(backup_file.toPath(), new File(path).toPath() );
+							
+							String path = storage_file.getPath().replaceAll(storage, backup);							
+							Files.copy(storage_file.toPath(), new File(path).toPath() );
+							Main.log.info("Добавлен новый файл: "+path);
+							storage_file = storage_it.next();
+						}
+						else
+						{
+							DateFormat formatter = new SimpleDateFormat("dd-MM-yyyy-HH:mm");
+						    String s = formatter.format(new Date(backup_file.lastModified()) );
+						    String name = backup_file.getName();
+						    name = name.substring(0, name.lastIndexOf('.')+1)+"DELETE."+s+'.'+name.substring(name.lastIndexOf('.')+1);
+						    
+						    String path = backup_file.getPath().replaceAll(backup, history.getPath());
+						    path = path.replaceAll(backup_file.getName(), name);
+						    
+						    Files.copy(backup_file.toPath(), new File(path).toPath());
+						    
+							Main.log.log(Level.SEVERE, "Файл из хранилища не совпал с файлом из бэкапа. Перемещен в папку истории : " + path);
+							backup_file = backup_it.next();
 						}
 					}
-					catch (Exception ex){}
-					
+					catch (Exception ex)
+					{
+						System.out.println(ex.toString());						
+					}
 				}
 			}
-		}
-		
-	/*	
-		for (File file : stg.listFiles())
-		{
-			if (file.isDirectory())
-			{
-				//checkoutFile(dirs.iterator(), file);
-				additionalBackup(new File(bp.getPath()+File.separator+file.getName()), file, new File(history.getPath()+File.separator+file.getName()));
-			}
-			else
-			{
-				// Если в конце файла нет ~ (знака временного файла), и файл новый, то копировать
-				if(true) //file.getName().substring(file.getName().length()-1) != "~" && checkoutFile(files.iterator(), file) )
-				{
-					DateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
-				    String s = formatter.format(new Date(file.lastModified()) );
-				    String name = null;
-//TODO проверка на удаленные файлы
-				    File f = new File(bp.getPath()+File.separator+file.getName());
-					try
-				    {
-						if (f.exists())
-						{
-							// Создаем имя для файла в history с датой переноса
-							name = file.getName().substring(0, file.getName().lastIndexOf('.')+1)+s+'.'+file.getName().substring(file.getName().lastIndexOf('.')+1);
-							
-							//Проверяем, есть ли папка history
-							if (!history.exists())
-							{
-								history.mkdirs();
-							}
-							
-							Files.copy(f.toPath(), new File(history.getPath()+File.separator+name).toPath() ); // Копирование из бэкапа в историю
-							f.delete();
-						}
-						Files.copy(file.toPath(), new File(bp.toPath()+File.separator+file.getName() ).toPath());//копирование в бэкап
-						System.out.println(file.getPath());
-				    }
-				    catch (Exception ex)
-				    {
-				    	ex.printStackTrace();
-				    	System.out.println( history.getPath()+File.separator+name);
-				    }
-				}
-			}
-		}
-		*/
+		}	
 	}
 	
 	/*
@@ -238,47 +211,26 @@ public class Backup {
 	{
 		for (File file : stg.listFiles())
 		{
-			//System.out.println(file.getPath());
-			
+		
 			if (file.isDirectory())
 			{
 				File next = new File(bp+File.separator+file.getName());
 				next.mkdir();
-				//System.out.println("directory "+next.getAbsolutePath());
 				copyFiles(next, file);
 			}
 			else
 			{
 				try
 				{
-					Files.copy(file.toPath(),new File(bp.toString()+File.separator+file.getName() ).toPath() ) ;
+					
+					char c = file.getName().charAt(file.getName().length()-1);
+					if ( c != '~') Files.copy(file.toPath(),new File(bp.toString()+File.separator+file.getName() ).toPath() ) ;
 				}
 				catch (Exception e)
 				{
-					//Main.log.log(Level.SEVERE, "Exception: Failed copy fail - "+file.toPath() +"\n"+ e);
+					Main.log.log(Level.SEVERE, "Exception: Failed copy fail - "+file.toPath() +"\n"+ e);
 				}	
 			}
 		}
-	
 	}
-	
-	private void save()
-	{
-		
-		try
-		{
-			File f = new File( 
-					backup.substring(0, backup.lastIndexOf(File.separator))+
-					File.separator+"backup.ser");
-			ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(f));
-			os.writeObject(this);
-			os.close();
-			Main.log.info("Saving completed");
-		}
-		catch (Exception ex)
-		{
-			//Main.log.log(Level.SEVERE, "Exception: Failed saving \n"+ ex);
-		}	
-	}
-
 }
--- src/Backup/Main.java
+++ src/Backup/Main.java
@@ -1,11 +1,7 @@
 package Backup;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.ObjectInputStream;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
-import java.util.logging.FileHandler;;
 
 public class Main {
 
@@ -14,11 +10,10 @@ public class Main {
 	 */
 	
 	static Logger log;
-	static LogManager lg;
-	
+		
 	public static void main(String[] args) 
 	{
-		
+		long start = System.nanoTime();
 		log = Logger.getLogger(Main.class.getName());
 		
 		try
@@ -27,14 +22,9 @@ public class Main {
 		}
 		catch (Exception ex)
 		{
-			
+			System.out.println("Не удалось загрузить настройки логирования");
 		}
 		
-		for(int i=0; i<args.length; i++ )
-		{
-			System.out.println(args[i]);
-			
-		}
 		System.out.println();
 		
 		
@@ -45,12 +35,14 @@ public class Main {
 		}
 		else
 		{
-			
-			//System.out.println(args[2]);
-			
 			Backup b = new Backup(args[0], args[1],  (args[2].equals("Full")) );
 			b.start();		
 		}
+		
+		long end = System.nanoTime();
+		long time = (end - start)/ 1000000 ;
+		System.out.println("Time: "+ time  );
+		
 	}
 
 }
